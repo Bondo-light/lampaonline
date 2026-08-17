12d99b6eaff86509d426dad5d33979a4012e8a56
Bohdan Kabanchuk <68802774+Bondo-light@users.noreply.github.com>
2026-08-17 14:25:21 +0300

    Update online.js

--- online.js
+++ online.js
@@ -1,12 +1,27 @@
 (function () {
-    var pluginUrl = 'http://cub.red/plugin/online';
-    
-    if (window.Lampa && Lampa.Utils && Lampa.Utils.putScript) {
-        Lampa.Utils.putScript([pluginUrl], function () {
-            console.log('Online plugin loaded via CUB');
-        }, function () {
-            // Резервный источник, если CUB временно недоступен
-            Lampa.Utils.putScript(['https://cdn.jsdelivr.net/gh/lampaplugins/store@main/plugins/online.js'], function () {});
-        });
+    'use strict';
+
+    function startPlugin() {
+        var mirrors = [
+            'https://lampac.sh/online.js',
+            'https://plugin.root34.ru/online.js',
+            'http://bwa.to/o'
+        ];
+
+        function tryLoad(index) {
+            if (index >= mirrors.length) return;
+            
+            Lampa.Utils.putScript([mirrors[index]], function () {
+                console.log('Online module loaded successfully from:', mirrors[index]);
+            }, function () {
+                tryLoad(index + 1);
+            });
+        }
+
+        tryLoad(0);
+    }
+
+    if (window.Lampa) {
+        startPlugin();
     }
 })();

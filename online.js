b298e60e8475578089743e4d2349a2f24b5c3234
Bohdan Kabanchuk <68802774+Bondo-light@users.noreply.github.com>
2026-08-17 14:30:56 +0300

    Update online.js

--- online.js
+++ online.js
@@ -1,35 +1,24 @@
 (function () {
     'use strict';
 
-    function OnlinePlugin(object) {
+    function Component(object) {
         var network = new Lampa.Reguest();
-        var scroll = new Lampa.Scroll({mask: true, over: true});
-        var items = [];
-        var html = $('<div></div>');
-        var body = $('<div class="online-list category-full"></div>');
+        var scroll  = new Lampa.Scroll({mask: true, over: true});
+        var files   = new Lampa.Files();
+        var filter  = new Lampa.Filter();
+        var html    = $('<div></div>');
+        var body    = $('<div class="category-full"></div>');
 
         this.create = function () {
             var _this = this;
             this.activity.loader(true);
-            
-            // Запрос к рабочему публичному балансеру
-            var url = 'https://api.allorigins.win/raw?url=' + encodeURIComponent('https://stream.voidboost.cc/embed/' + object.movie.kinopoisk_id);
-            
-            network.silent(url, function (found) {
-                _this.activity.loader(false);
-                var iframe = $('<iframe src="https://vidsrc.me/embed/movie?imdb=' + (object.movie.imdb_id || '') + '" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>');
-                html.append(iframe);
-            }, function () {
-                _this.empty();
-            });
 
-            return this.render();
-        };
-
-        this.empty = function () {
-            this.activity.loader(false);
-            var empty = $('<div class="empty__title">Источники не найдены или требуют настройки парсера</div>');
+            // Отрисовка базового контейнера источников
+            var empty = $('<div class="empty center"><div class="empty__title">Поиск видеопотоков...</div><div class="empty__descr">Если список не загрузился, проверьте настройки парсера или прокси в CUB.</div></div>');
             html.append(empty);
+
+            this.activity.loader(false);
+            return this.render();
         };
 
         this.render = function () {
@@ -37,32 +26,34 @@
         };
     }
 
-    // Регистрация кнопки "Онлайн" в Lampa
-    function init() {
-        if (window.online_plugin_inited) return;
-        window.online_plugin_inited = true;
+    function start() {
+        // Добавление компонента в систему Lampa
+        Lampa.Component.add('online_mod_custom', Component);
 
-        Lampa.Component.add('online_mod', OnlinePlugin);
-
+        // Внедрение кнопки "Онлайн" в карточку фильма
         Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'complite') {
+            if (e.type === 'complite') {
                 var render = e.object.activity.render();
-                var btn = $('<div class="full-start__button selector button--online"><svg height="24" viewBox="0 0 24 24" width="24"><path d="M8 5v14l11-7z"/><path d="0 0h24v24H0z" fill="none"/></svg><span>Онлайн</span></div>');
+                
+                // Проверяем, чтобы кнопка не дублировалась
+                if (render.find('.button--online-custom').length === 0) {
+                    var btn = $('<div class="full-start__button selector button--online-custom"><svg height="24" viewBox="0 0 24 24" width="24" fill="none" stroke="currentColor" stroke-width="2"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg><span>Онлайн</span></div>');
 
-                btn.on('hover:enter', function () {
-                    Lampa.Activity.push({
-                        title: 'Онлайн просмотр',
-                        component: 'online_mod',
-                        movie: e.object.method == 'tv' ? e.object.card : e.object.card
+                    btn.on('hover:enter', function () {
+                        Lampa.Activity.push({
+                            title: 'Онлайн',
+                            component: 'online_mod_custom',
+                            movie: e.object.card
+                        });
                     });
-                });
 
-                render.find('.full-start__buttons').append(btn);
+                    render.find('.full-start__buttons').append(btn);
+                }
             }
         });
     }
 
     if (window.Lampa) {
-        init();
+        start();
     }
 })();
